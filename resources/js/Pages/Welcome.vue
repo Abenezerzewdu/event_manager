4f4f0000462666801a8ebd5e5fd8a60cb278da53
abenezer <abenezerzewdu11@gmail.com>
2025-08-12 07:08:19 +0300

    Initial Laravel + Vue + Inertia setup

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="text-2xl text-center mt-10">
+            Hello from Laravel + Vue + Inertia 🎉
+        </h1>
+        <p class="text-center mt-4">
+            This is a simple event management application.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Welcome",
+};
+</script>
